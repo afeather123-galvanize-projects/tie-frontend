ecc52793fac166ef159c2faa6f4e2dc978102917
afeather123 <afeather@asu.edu>
2018-07-20 10:51:40 -0700

    fix login page

--- communify-frontend/src/components/login/Login.js
+++ communify-frontend/src/components/login/Login.js
@@ -19,7 +19,7 @@ class Login extends React.Component {
             <Container style={{height: '80vh', position: 'relative'}}>
                 <Row style={{margin: 0, position: 'absolute', top: '50%', transform: 'translate(0%, -50%)'}}>
                     <Col sm="8">
-                    <div>
+                    <div style={{paddingTop: '10%'}}>
                     <Jumbotron>
                         <h1 className="display-3">Welcome to Tie!</h1>
                         <p className="lead">A site where you can plan coordinate and keep in touch with the groups and events you care about the most.</p>
@@ -31,7 +31,8 @@ class Login extends React.Component {
                     </div>
                     </Col>
                     <Col sm="4">
-                        <Form onSubmit={handleSubmit}>
+                        <h2>Login</h2>
+                        <Form onSubmit={handleSubmit} className="mb-2">
                             <FormGroup>
                                 <Label for="email">Email</Label>
                                 <Input id="email" 
@@ -44,6 +45,36 @@ class Login extends React.Component {
                                 <Input id="password" type="password" 
                                 placeholder="john@doe.com" 
                                 onChange={e=>this.setState({password:e.target.value})}/>
+                            </FormGroup>
+                            <Button>Submit</Button>
+                        </Form>
+                        <h2>Signup</h2>
+                        <Form onSubmit={handleSubmit}>
+                            <FormGroup>
+                                <Label for="email">Email</Label>
+                                <Input id="email" 
+                                type="email" 
+                                placeholder="john@doe.com" 
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="name">Username</Label>
+                                <Input id="name" 
+                                type="text" 
+                                placeholder="Username" 
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="password">Password</Label>
+                                <Input id="password" type="password" 
+                                placeholder="Password" 
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="password">Confirm Password</Label>
+                                <Input id="password" type="password" 
+                                placeholder="Confirm Password" 
+                                />
                             </FormGroup>
                             <Button>Submit</Button>
                         </Form>
